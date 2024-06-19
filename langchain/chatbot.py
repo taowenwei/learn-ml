56b31fb75513730a137e7c8d5f8d149882d38966
wtao <wtao@macbook>
2024-06-19 11:32:02 -0700

    chatbot

--- langchain/chatbot.py
+++ langchain/chatbot.py
@@ -4,6 +4,7 @@ from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage
+from datetime import datetime
 
 # llm
 model = ChatOpenAI(model="gpt-3.5-turbo")
@@ -20,7 +21,7 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-# Chat history
+# Chat history, supporting multiple sessions
 store = {}
 
 
@@ -35,11 +36,17 @@ chain = prompt | model
 
 # chain with chat history
 chatbot = RunnableWithMessageHistory(chain, get_session_history)
-
-# stream out llm output as it generates tokens (instead of printing after all tokens are generated)
-for r in chatbot.stream(
-    {"messages": [HumanMessage(
-        content="can you summarize star wars: clone war?")]},
-        config={"configurable": {"session_id": "abc2"}}):
-    print(r.content, end="")
+session_id = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+while True:
+    user_input = input("You: ")
+    if user_input == '.exit':
+        print('bye bye')
+        break
+    # stream out llm output as it generates tokens (instead of printing after all tokens are generated)
+    for r in chatbot.stream(
+        {"messages": [HumanMessage(
+            content=user_input)]},
+            config={"configurable": {"session_id": session_id}}):
+        print(r.content, end="")
+    print()
 print()

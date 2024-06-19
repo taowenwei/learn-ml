ec8257eae8775711ed263b0af75af040236db7e7
wtao <wtao@macbook>
2024-06-19 15:01:10 -0700

    more simple

--- langchain/agent-simple.py
+++ langchain/agent-simple.py
@@ -24,14 +24,22 @@ def execute(agent, question, config):
             message.pretty_print()
 
 
+# add tool
 tools = [get_weather]
 model = ChatOpenAI(model="gpt-3.5-turbo")
+# add memory
 memory = SqliteSaver.from_conn_string(":memory:")
 config = {"configurable": {"thread_id": "1234"}}
+# add simple prompt
 system_prompt = "You are a helpful bot named Fred. Only use the `get-weather` tool when weather is asked"
+# add interrupt before executing a tool
+# interrupt_before = ["tools"]
 
 agent = create_react_agent(
-    model, tools, messages_modifier=system_prompt, checkpointer=memory)
+    model, tools, #interrupt_before=interrupt_before, 
+    messages_modifier=system_prompt, checkpointer=memory)
+# add 10 sec timeout
+agent.step_timeout = 10 
 execute(agent, 'my name is wenwei. i live in san jose, CA', config)
 execute(agent, 'whats the weather in my town?', config)
 execute(agent, 'whats my name?', config)

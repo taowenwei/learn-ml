980770a0e8ae9b3da5049b681ff33b5d0e48bde8
wtao <wtao@macbook>
2024-06-20 21:36:34 -0700

    react impl

--- langchain/agent-python.py
+++ langchain/agent-python.py
@@ -6,7 +6,7 @@ tools = [PythonREPLTool()]
 model = ChatOpenAI(temperature=0)
 
 
-def agentExecutorImpl():
+def agentExecutorImpl(question):
     from langchain.agents import AgentExecutor
     from langchain.agents import create_openai_functions_agent
 
@@ -21,8 +21,52 @@ def agentExecutorImpl():
     prompt = base_prompt.partial(instructions=instructions)
     agent = create_openai_functions_agent(model, tools, prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-    return agent_executor
+    agent_executor.invoke({"input": question})
 
 
-agent = agentExecutorImpl()
-agent.invoke({"input": "What is the 10th fibonacci number?"})
+
+def reactAgentImpl(question):
+    from langgraph.checkpoint.sqlite import SqliteSaver
+    from langgraph.prebuilt import create_react_agent
+    from langchain_core.messages import HumanMessage
+
+    def execute(agent, question, config):
+        for chunk in agent.stream(
+            {'messages': [HumanMessage(
+                content=question)]},
+                config, stream_mode='values'
+        ):
+            message = chunk['messages'][-1]
+            if isinstance(message, tuple):
+                print(message)
+            else:
+                message.pretty_print()
+
+    # add memory
+    memory = SqliteSaver.from_conn_string(':memory:')
+    config = {'configurable': {'thread_id': '1234'}}
+    system_prompt = '''
+        You are an agent designed to write and execute python code to answer questions.
+        You have access to a python REPL tool, which you can use to execute python code.
+
+        When answer a question, following the steps below and print out your action in each step,
+
+        Question: the input question you must answer
+        Thought: you should always think about what to do
+        Action: the action on the tool
+        Action Input: the input to the action
+        Observation: the result of the action
+
+        ... (this Thought/Action/Action Input/Observation can repeat N times)
+
+        Finally: I now know the final answer to the original input question
+        '''
+
+    agent = create_react_agent(
+        model, tools, messages_modifier=system_prompt, checkpointer=memory)
+    execute(agent, question, config)
+
+
+question = "What is the 10th fibonacci number?"
+agentExecutorImpl(question)
+#reactAgentImpl(question)

72b4a1f831c2b8a2ab317dc6dde96b5b3820cd0d
wtao <wtao@macbook>
2024-06-21 05:58:52 -0700

    python

--- langchain/agent-python.py
+++ langchain/agent-python.py
@@ -1,14 +1,15 @@
 from langchain import hub
 from langchain_openai import ChatOpenAI
-from langchain_experimental.tools import PythonREPLTool
 
-tools = [PythonREPLTool()]
 model = ChatOpenAI(temperature=0)
 
 
 def agentExecutorImpl(question):
     from langchain.agents import AgentExecutor
     from langchain.agents import create_openai_functions_agent
+    from langchain_experimental.tools import PythonREPLTool
+
+    tools = [PythonREPLTool()]
 
     instructions = """You are an agent designed to write and execute python code to answer questions.
     You have access to a python REPL, which you can use to execute python code.
@@ -19,6 +20,7 @@ def agentExecutorImpl(question):
     """
     base_prompt = hub.pull("langchain-ai/openai-functions-template")
     prompt = base_prompt.partial(instructions=instructions)
+
     agent = create_openai_functions_agent(model, tools, prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
     agent_executor.invoke({"input": question})
@@ -28,6 +30,8 @@ def reactAgentImpl(question):
     from langgraph.checkpoint.sqlite import SqliteSaver
     from langgraph.prebuilt import create_react_agent
     from langchain_core.messages import HumanMessage
+    from langchain_core.messages import HumanMessage
+    from langchain_core.tools import StructuredTool
 
     def execute(agent, question, config):
         for chunk in agent.stream(
@@ -41,24 +45,20 @@ def reactAgentImpl(question):
             else:
                 message.pretty_print()
 
-    # add memory
+    def run_python(query: str) -> str:
+        '''Return the python code for the specified query.'''
+        return 0
+
+    tools = [StructuredTool.from_function(
+        func=run_python, handle_tool_error=True, )]
+
     memory = SqliteSaver.from_conn_string(':memory:')
     config = {'configurable': {'thread_id': '1234'}}
     system_prompt = '''
-        You are an agent designed to write and execute python code to answer questions.
-        You have access to a python REPL tool, which you can use to execute python code.
-
-        When answer a question, following the steps below and print out your action in each step,
-
-        Question: the input question you must answer
-        Thought: you should always think about what to do
-        Action: the action on the tool
-        Action Input: the input to the action
-        Observation: the result of the action
-
-        ... (this Thought/Action/Action Input/Observation can repeat N times)
-
-        Finally: I now know the final answer to the original input question
+        Following the steps to answer a question,
+        1. generate a python code solution based on the question.
+        2. run the `run_python` tool. 
+        3. ALWAYS use the tool result as the final answer
         '''
 
     agent = create_react_agent(

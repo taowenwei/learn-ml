f9392cb68fd99dab5dea3145fbb8060ba57b92ef
wtao <wtao@macbook>
2024-06-26 18:36:40 -0700

    fast api

--- fakeService/fake-backend.py
+++ fakeService/fake-backend.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request, HTTPException, status
 from typing import List, Optional
 import uvicorn
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -12,9 +14,24 @@ movies = [
     # Add more movies here
 ]
 
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
 @app.get("/movies", response_model=List[dict])
-def get_movies():
+def get_movies(year: int = None):
+    if year != None:
+        movies_by_year = [movie for movie in movies if movie["year"] == year]
+        if not movies_by_year:
+            raise HTTPException(
+                status_code=404, detail="No movies found for the given year")
+        return movies_by_year
     return movies
+
 
 @app.get("/movies/{id}", response_model=dict)
 def get_movie_by_id(id: int):
@@ -23,16 +40,11 @@ def get_movie_by_id(id: int):
         raise HTTPException(status_code=404, detail="Movie not found")
     return movie
 
-@app.get("/years", response_model=List[int])
+
+@app.get("/movies/years/", response_model=List[int])
 def get_movie_years():
     return list(set(movie["year"] for movie in movies))
 
-@app.get("/movies/years/{year}", response_model=List[dict])
-def get_movies_by_year(year: int):
-    movies_by_year = [movie for movie in movies if movie["year"] == year]
-    if not movies_by_year:
-        raise HTTPException(status_code=404, detail="No movies found for the given year")
-    return movies_by_year
 
 @app.post("/movies", response_model=dict)
 def create_movie(title: str, year: int):
@@ -41,5 +53,6 @@ def create_movie(title: str, year: int):
     movies.append(new_movie)
     return new_movie
 
+# http://127.0.0.1:4000/openapi.json for Open API spec
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=4000)

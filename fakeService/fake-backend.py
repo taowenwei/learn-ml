8b0655bde0a0fa783911b64469db517f0e3035c9
wtao <wtao@macbook>
2024-06-29 14:55:44 -0700

    more api example

--- fakeService/fake-backend.py
+++ fakeService/fake-backend.py
@@ -17,14 +17,31 @@ movies = [
     # Add more movies here
 ]
 
+comments = [
+    {"movieId": 1, "comments": [
+        {"id": 1, "comment": "Undeniably powerful and moving"},
+        {"id": 2, "comment": "Effortless screen presence lends Shawshank the closest thing to credibility it can manage"}]},
+    {"movieId": 2, "comments": [
+        {"id": 1, "comment": "If ever there was a great example of how the best popular movies come out of a merger of commerce and art"},
+        {"id": 2, "comment": "The best film of 1972"}]},
+    {"movieId": 3, "comments": [
+        {"id": 1, "comment": "American movies have entered a new era of ambivalence when it comes to their heroes or maybe just superness"},
+        {"id": 2, "comment": "somebody who wants to just tear down the world around him"}]}
+]
+
 
 class Movie(BaseModel):
     id: int = None
     title: str
     year: int
+    Comments: list = None
+
+class Comment(BaseModel):
+    id: int = None
+    comment: str
 
 
-@app.get("/movies", response_model=List[Movie], summary="Get all movies or all movies of a given release year")
+@ app.get("/movies", response_model=List[Movie], summary="Get all movies or all movies of a given release year")
 def get_movies(year: int = None):
     if year != None:
         movies_by_year = [movie for movie in movies if movie["year"] == year]
@@ -35,7 +52,7 @@ def get_movies(year: int = None):
     return movies
 
 
-@app.get("/movies/{id}", response_model=Movie, summary='Get a movie by its Id')
+@ app.get("/movies/{id}", response_model=Movie, summary='Get a movie by its Id')
 def get_movie_by_id(id: int):
     movie = next((movie for movie in movies if movie["id"] == id), None)
     if not movie:
@@ -43,12 +60,25 @@ def get_movie_by_id(id: int):
     return movie
 
 
-@app.get("/movies/years/", response_model=List[int], summary='Get all movie release years')
+@ app.get("/movies/{id}/comments/{cid}", response_model=Comment, summary='From a movie of id, get one of its comments of cid')
+def get_movie_by_id_comment_by_cid(id: int, cid: int):
+    movieComments = next(
+        (movieComments for movieComments in comments if movieComments["movieId"] == id), None)
+    if not movieComments:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    comment = next(
+        (comment for comment in movieComments['comments'] if comment["id"] == cid), None)
+    if not comment:
+        raise HTTPException(status_code=404, detail="Comment not found")
+    return comment
+
+
+@ app.get("/movies/years/", response_model=List[int], summary='Get all movie release years')
 def get_movie_years():
     return list(set(movie["year"] for movie in movies))
 
 
-@app.post("/movies", response_model=Movie, summary='Get all movie release years')
+@ app.post("/movies", response_model=Movie, summary='Get all movie release years')
 def create_movie(movie: Movie):
     movie_id = len(movies) + 1
     new_movie = {"id": movie_id, "title": movie.title, "year": movie.year}
@@ -66,7 +96,9 @@ def toOpenAPISpec():
             routes=app.routes,
         ), f)
 
+# http://127.0.0.1:4000/openapi.json for Open API spec, or just call below
 
-# http://127.0.0.1:4000/openapi.json for Open API spec
+# toOpenAPISpec()
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=4000)

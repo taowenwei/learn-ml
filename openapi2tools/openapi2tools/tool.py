2f8b69dcb4e06e06513f99c07651db55b1ff6917
wtao <wtao@macbook>
2024-06-29 17:27:31 -0700

    code gen

--- openapi2tools/openapi2tools/tool.py
+++ openapi2tools/openapi2tools/tool.py
@@ -5,7 +5,7 @@ import sys
 def indentCode(indent, lines):
     # only indent from the 2nd line
     indented = [lines[0],  *
-            (list(map(lambda line: ' ' * indent + line, lines[1:])))]
+                (list(map(lambda line: ' ' * indent + line, lines[1:])))]
     return '\n'.join(indented)
 
 
@@ -25,100 +25,6 @@ def genType(dtype):
             raise RuntimeError(f'unexpected data type {dtype}')
 
 
-def genToolParameters(methodBody, components):
-    def codegen(name, dtype, required=True):
-        if required:
-            return f'{name}: {genType(dtype)}'
-        return f'{name}: Optional[{genType(dtype)}]'
-
-    if 'parameters' in methodBody:
-        paramList = list(map(lambda parameter: codegen(
-            parameter['name'], parameter['schema']['type'], parameter['required']), methodBody['parameters']))
-        return f"{', '.join(paramList)}"
-
-    if 'requestBody' in methodBody:
-        schema = methodBody['requestBody']['content']['application/json']['schema']['$ref']
-        schema = schema[schema.rfind('/') + 1:]
-        schemaDef = components['schemas'][schema]
-        properties = list(filter(
-            lambda property: property in schemaDef['required'], schemaDef['properties'].keys()))
-        paramList = list(map(lambda property: codegen(
-            property, schemaDef['properties'][property]['type']), properties))
-        return f"{', '.join(paramList)}"
-
-    return ''
-
-
-def genToolReturn(methodBody):
-    schema = methodBody['responses']['200']['content']['application/json']['schema']
-    if 'type' in schema:
-        return genType(schema['type'])
-    if '$ref' in schema:
-        return 'dict'
-    raise RuntimeError(f'unexpected tool return data type {schema}')
-
-
-def genHttpPath(path, methodBody):
-    if 'parameters' in methodBody:
-        parameters = list(
-            filter(lambda parameter: parameter['in'] == 'query', methodBody['parameters']))
-        queries = list(map(lambda parameter:
-                           "f'" + parameter['name'] + '={' + parameter['name'] + '}' + '\' if ' + parameter['name'] + " != None else ''", parameters))
-        queries = list(filter(lambda query: query != '', queries))
-        queries = list(map(lambda query: f'({query})', queries))
-        if len(queries) > 0:
-            return f"'{path}?' + " + " + '&' + ".join(queries)
-    return f"'{path}'"
-
-
-def genHttpBody(indent, methodBody, components):
-    if 'requestBody' in methodBody and methodBody['requestBody']['required']:
-        schema = methodBody['requestBody']['content']['application/json']['schema']['$ref']
-        schema = schema[schema.rfind('/') + 1:]
-        schemaDef = components['schemas'][schema]
-        properties = list(filter(
-            lambda property: property in schemaDef['required'], schemaDef['properties'].keys()))
-        paramList = list(
-            map(lambda property: f"{' ' * 4}'{property}': {property}, ", properties))
-        lines = ['', 'data = {', *paramList, '}']
-        return indentCode(indent, lines)
-    return ''
-
-
-def genSuccess(indent):
-    return ''
-
-
-def genFailure(indent):
-    return ''
-
-
-def genTool(clzName, path, method, methodBody, components):
-    print(f'    generating tool for {path} {method}')
-
-    tool = f"""
-    @tool
-    def {methodBody['operationId']}({genToolParameters(methodBody, components)}) -> {genToolReturn(methodBody)}:
-        '''{methodBody['summary']}'''
-        {genHttpBody(8, methodBody, components)}
-        response = requests.{method}({clzName}.BaseUrl + {genHttpPath(path, methodBody)}, headers={clzName}.HttpHeader{', json=data' if 'requestBody' in methodBody and methodBody['requestBody']['required'] else ''})
-        {genSuccess(8)}
-        {genFailure(8)}
-        return response.json()
-    """
-    lines = tool.split('\n')[1:]
-    return '\n'.join(lines)
-
-
-def genPath(clzName, pathName, pathBody, components):
-    print(f'generating {pathName}')
-
-    methods = pathBody.keys()
-    tools = list(map(lambda method: genTool(
-        clzName, pathName, method, pathBody[method], components), methods))
-    return tools
-
-
 def genToolCapabilities(indent, paths):
     def genCapability(pathBody):
         methods = pathBody.keys()
@@ -130,6 +36,114 @@ def genToolCapabilities(indent, paths):
     return indentCode(indent, capabilities)
 
 
+def genTools(indent, clzName, paths, components):
+    def genToolParameters(methodBody, components):
+        def codegen(name, dtype, required=True):
+            if required:
+                return f'{name}: {genType(dtype)}'
+            return f'{name}: Optional[{genType(dtype)}]'
+
+        if 'parameters' in methodBody:
+            paramList = list(map(lambda parameter: codegen(
+                parameter['name'], parameter['schema']['type'], parameter['required']), methodBody['parameters']))
+            return f"{', '.join(paramList)}"
+
+        if 'requestBody' in methodBody:
+            schema = methodBody['requestBody']['content']['application/json']['schema']['$ref']
+            schema = schema[schema.rfind('/') + 1:]
+            schemaDef = components['schemas'][schema]
+            properties = list(filter(
+                lambda property: property in schemaDef['required'], schemaDef['properties'].keys()))
+            paramList = list(map(lambda property: codegen(
+                property, schemaDef['properties'][property]['type']), properties))
+            return f"{', '.join(paramList)}"
+
+        return ''
+
+    def genToolReturn(methodBody):
+        schema = methodBody['responses']['200']['content']['application/json']['schema']
+        if 'type' in schema:
+            return genType(schema['type'])
+        if '$ref' in schema:
+            return 'dict'
+        raise RuntimeError(f'unexpected tool return data type {schema}')
+
+    def genHttpPath(path, methodBody):
+        if 'parameters' in methodBody:
+            parameters = list(
+                filter(lambda parameter: parameter['in'] == 'query', methodBody['parameters']))
+            queries = list(map(lambda parameter:
+                               "f'" + parameter['name'] + '={' + parameter['name'] + '}' + '\' if ' + parameter['name'] + " != None else ''", parameters))
+            queries = list(filter(lambda query: query != '', queries))
+            queries = list(map(lambda query: f'({query})', queries))
+            if len(queries) > 0:
+                return f"'{path}?' + " + " + '&' + ".join(queries)
+        return f"'{path}'"
+
+    def genHttpBody(indent, methodBody, components):
+        if 'requestBody' in methodBody and methodBody['requestBody']['required']:
+            schema = methodBody['requestBody']['content']['application/json']['schema']['$ref']
+            schema = schema[schema.rfind('/') + 1:]
+            schemaDef = components['schemas'][schema]
+            properties = list(filter(
+                lambda property: property in schemaDef['required'], schemaDef['properties'].keys()))
+            paramList = list(
+                map(lambda property: f"{' ' * 4}'{property}': {property}, ", properties))
+            lines = ['', 'data = {', *paramList, '}']
+            return indentCode(indent, lines)
+        return ''
+
+    def genSuccess(indent):
+        return ''
+
+    def genFailure(indent):
+        return ''
+
+    def genTool(clzName, path, method, methodBody, components):
+        print(f'    generating tool for {path} {method}')
+
+        tool = f"""
+@tool
+def {methodBody['operationId']}({genToolParameters(methodBody, components)}) -> {genToolReturn(methodBody)}:
+    '''{methodBody['summary']}'''
+    {genHttpBody(4, methodBody, components)}
+    response = requests.{method}({clzName}.BaseUrl + {genHttpPath(path, methodBody)}, headers={clzName}.HttpHeader{', json=data' if 'requestBody' in methodBody and methodBody['requestBody']['required'] else ''})
+    {genSuccess(4)}
+    {genFailure(4)}
+    return response.json()
+        """
+        lines = tool.split('\n')[1:]
+        return '\n'.join(lines)
+
+    def genPath(clzName, pathName, pathBody, components):
+        print(f'generating {pathName}')
+
+        methods = pathBody.keys()
+        tools = list(map(lambda method: genTool(
+            clzName, pathName, method, pathBody[method], components), methods))
+        return tools
+
+    tools = list(map(lambda key: genPath(
+        clzName, key, paths[key], components), paths.keys()))
+    tools = sum(tools, [])
+    tools = list(map(lambda tool: tool.split('\n'), tools))
+    tools = sum(tools, [])
+    return indentCode(indent, tools)
+
+
+def genToolNames(indent, paths):
+    def genPath(pathBody):
+        methods = pathBody.keys()
+        tools = list(
+            map(lambda method: f"{pathBody[method]['operationId']},", methods))
+        return tools
+
+    tools = list(map(lambda key: genPath(
+        paths[key]), paths.keys()))
+    tools = sum(tools, [])
+    return indentCode(indent, tools)
+
+
 def convert(apiSpec):
     spec = None
     with open(apiSpec, 'r') as file:
@@ -139,16 +153,12 @@ def convert(apiSpec):
     components = spec['components']
     clzName = spec['info']['title'] + 'Api'
 
-    tools = list(map(lambda key: genPath(
-        clzName, key, paths[key], components), paths.keys()))
-    tools = sum(tools, [])
-    toolCode = '\n'.join(tools)
-
     clzBody = f"""
 from typing import Optional
 import requests
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.tools import tool
+from langchain_core.runnables import RunnableConfig
 
 class {clzName}:
 
@@ -171,11 +181,23 @@ class {clzName}:
         ),
         ('user', '{{user}}'),
     ])
+
+    {genTools(4, clzName, paths, components)}
+
+    tools = [
+        {genToolNames(8, paths)}
+    ]
+
+    def __init__(self, llm):
+        self.runnable = {clzName}.prompt | llm.bind_tools({clzName}.tools)
+
+    def __call__(self, state, config: RunnableConfig):
+        return {{'messages': [self.runnable.invoke(state['messages'])]}}
     """
 
     lines = clzBody.split('\n')[1:]
     with open('output/abcdef.py', 'w') as wf:
-        wf.write('\n'.join(lines) + '\n' + toolCode)
+        wf.write('\n'.join(lines))
 
 
 if __name__ == '__main__':

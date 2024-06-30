410a43cafa7d312382c88f588a8add15eccdb773
wtao <wtao@macbook>
2024-06-29 17:30:20 -0700

    gtest code gen

--- langgraph/movies.py
+++ langgraph/movies.py
@@ -11,73 +11,20 @@ from langchain_openai import ChatOpenAI
 from langchain_core.runnables import Runnable, RunnableConfig
 
 
-class Assistant:
-    BaseUrl = 'http://localhost:4000'
+from typing import Optional
+import requests
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import tool
+from langchain_core.runnables import RunnableConfig
+import moviesApi
 
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                'system',
-                '''You are a specialized assistant for movie records. You can,
-                1. use the `get_movies_movies_get` tool to get all movies or all movies of a release year
-                2. use the `get_movie_by_id_movies__id__get` tool to get a movie by its id
-                3. use the `get_movie_years_movies_years__get` tool to get all movie release years
-                4. use the `create_movie_movies_post` tool to create a new movie
-                
-                Now answer your question'''
-            ),
-            ('user', '{user}'),
-        ])
 
-    @tool
-    def get_movies_movies_get(year: Optional[int] = None) -> str:
-        '''Get all movies or movies by a release year'''
-
-        url = f'{Assistant.BaseUrl}/movies' + \
-            (f'?year={year}' if year != None else '')
-        response = requests.get(url)
-        return response.json()
-
-    @tool
-    def get_movie_by_id_movies__id__get(id: int) -> dict:
-        '''Get movie by Id'''
-
-        response = requests.get(f'{Assistant.BaseUrl}/movies/{id}')
-        return response.json()
-
-    @tool
-    def get_movie_years_movies_years__get(year: Optional[int] = None) -> list[int]:
-        '''Get all moovie release years'''
-
-        response = requests.get(f'{Assistant.BaseUrl}/movies/years/')
-        return response.json()
-
-    @tool
-    def create_movie_movies_post(title: str, year: int) -> dict:
-        '''Create a new movie'''
-
-        response = requests.post(f'{Assistant.BaseUrl}/movies', json={
-            'title': title,
-            'year': year
-        })
-        return response.json()
-
-    tools = [get_movies_movies_get,
-             get_movie_by_id_movies__id__get,
-             get_movie_years_movies_years__get,
-             create_movie_movies_post]
-
-    def __init__(self):
-        llm = ChatOpenAI(model='gpt-3.5-turbo')
-        self.runnable = Assistant.prompt | llm.bind_tools(Assistant.tools)
-
-    def __call__(self, state: utils.State, config: RunnableConfig):
-        return {'messages': [self.runnable.invoke(state['messages'])]}
-
+llm = ChatOpenAI(model='gpt-3.5-turbo')
 
 builder = StateGraph(utils.State)
-builder.add_node('movieAssistant', Assistant())
-builder.add_node('tools', utils.toolNodeWithFallback(Assistant.tools))
+builder.add_node('movieAssistant', moviesApi.MoviesApi(llm))
+builder.add_node('tools', utils.toolNodeWithFallback(
+    moviesApi.MoviesApi.tools))
 builder.add_conditional_edges(
     'movieAssistant',
     tools_condition,
@@ -97,4 +44,3 @@ config = {'configurable': {'thread_id': '1'}}
 # utils.graphStream(graph, config, user_input)
 user_input = 'can you add a new movie with title="Despicable Me 4", year=2024?'
 utils.graphStream(graph, config, user_input)
-
---
+++ langgraph/moviesApi.py
@@ -0,0 +1,96 @@
+from typing import Optional
+import requests
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import tool
+from langchain_core.runnables import RunnableConfig
+
+class MoviesApi:
+
+    BaseUrl = 'http://localhost:4000'
+
+    HttpHeader = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer YOUR_ACCESS_TOKEN'
+        }
+
+    prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            'system',
+            '''You are a specialized manager movie records. You can,
+
+            + use the `get_movies_movies_get` tool to get all movies or all movies of a given release year
+            + use the `create_movie_movies_post` tool to get all movie release years
+            + use the `get_movie_by_id_movies__id__get` tool to get a movie by its id
+            + use the `get_movie_by_id_comment_by_cid_movies__id__comments__cid__get` tool to from a movie of id, get one of its comments of cid
+            + use the `get_movie_years_movies_years__get` tool to get all movie release years
+            
+            Now answer your question'''
+        ),
+        ('user', '{user}'),
+    ])
+
+    @tool
+    def get_movies_movies_get(year: Optional[int], sort: Optional[bool]) -> list:
+        '''Get all movies or all movies of a given release year'''
+        
+        response = requests.get(MoviesApi.BaseUrl + '/movies?' + (f'year={year}' if year != None else '') + '&' + (f'sort={sort}' if sort != None else ''), headers=MoviesApi.HttpHeader)
+        
+        
+        return response.json()
+            
+    @tool
+    def create_movie_movies_post(title: str, year: int) -> dict:
+        '''Get all movie release years'''
+        
+        data = {
+            'title': title, 
+            'year': year, 
+        }
+        response = requests.post(MoviesApi.BaseUrl + '/movies', headers=MoviesApi.HttpHeader, json=data)
+        
+        
+        return response.json()
+            
+    @tool
+    def get_movie_by_id_movies__id__get(id: int) -> dict:
+        '''Get a movie by its Id'''
+        
+        response = requests.get(MoviesApi.BaseUrl + '/movies/{id}', headers=MoviesApi.HttpHeader)
+        
+        
+        return response.json()
+            
+    @tool
+    def get_movie_by_id_comment_by_cid_movies__id__comments__cid__get(id: int, cid: int) -> dict:
+        '''From a movie of id, get one of its comments of cid'''
+        
+        response = requests.get(MoviesApi.BaseUrl + '/movies/{id}/comments/{cid}', headers=MoviesApi.HttpHeader)
+        
+        
+        return response.json()
+            
+    @tool
+    def get_movie_years_movies_years__get() -> list:
+        '''Get all movie release years'''
+        
+        response = requests.get(MoviesApi.BaseUrl + '/movies/years/', headers=MoviesApi.HttpHeader)
+        
+        
+        return response.json()
+            
+
+    tools = [
+        get_movies_movies_get,
+        create_movie_movies_post,
+        get_movie_by_id_movies__id__get,
+        get_movie_by_id_comment_by_cid_movies__id__comments__cid__get,
+        get_movie_years_movies_years__get,
+    ]
+
+    def __init__(self, llm):
+        self.runnable = MoviesApi.prompt | llm.bind_tools(MoviesApi.tools)
+
+    def __call__(self, state, config: RunnableConfig):
+        return {'messages': [self.runnable.invoke(state['messages'])]}
+    
